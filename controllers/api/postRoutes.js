463fba8870942edae4fa80e89c34242c45d64d23
kbario <kylebario1@gmail.com>
2022-04-06 11:23:32 +0800

    added route for edit functionality

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
 
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get('/edit/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      attributes: ['id', 'title', 'content'],
+    });
+    const posts = postData.get({ plain: true });
+
+    res.render('edit-post', posts);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const newPost = await Post.create({
@@ -8,6 +31,22 @@ router.post('/', async (req, res) => {
       content: req.body.content,
       user_id: req.session.user_id,
     });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const newPost = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      { where: { id: req.params.id } }
+    );
 
     res.status(200).json(newPost);
   } catch (err) {
